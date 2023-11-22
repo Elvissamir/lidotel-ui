0db5d368a3b8d5c85c010e70c5af761a8e5a13e7
elvissamir <elvissamir18@gmail.com>
2023-11-22 09:15:03 -0400

    Add config to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-fetch(`${import.meta.env.BASE_URL || './'}config.json`).then(resp => resp.json()).then(config => {
-  console.log(`basePath is ${config.app.basePath}`)
+const config = {
+  "api": {
+    "baseUri": "http://localhost:5086"
+  },
+  "app": {
+    "name": "The Exchange",
+    "subtitle": "",
+    "basePath": "",
+    "showEmailPage": false,
+    "showNotificationPage": false
+  }
+}
 
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode>
-      <BrowserRouter basename={config.app.basePath}>
-        <App appConfig={config} />
-      </BrowserRouter>
-    </React.StrictMode>)
-}).catch(err => console.log("The config file fetching failed: ", err))
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode>
+    <BrowserRouter basename={config.app.basePath}>
+      <App appConfig={config} />
+    </BrowserRouter>
+</React.StrictMode>)
