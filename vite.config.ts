5bcaee59107fffa1201f665d38232fba06b3dbe5
elvissamir <elvissamir18@gmail.com>
2023-11-22 08:59:03 -0400

    Edit

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(({ mode }) => {
   return {
-    base: '/lidotel-ui/',
-    server: {
-      port: +(process.env.PORT || 8598)
-    },
     plugins: [
       react(),
     ]
