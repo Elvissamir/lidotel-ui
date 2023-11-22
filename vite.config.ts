5d552efb82246073fde19ec3af4267a29ff82479
elvissamir <elvissamir18@gmail.com>
2023-11-22 08:26:59 -0400

    Add base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(({ mode }) => {
   return {
-    base: '/',
+    base: '/lidotel-ui',
     server: {
       port: +(process.env.PORT || 8598)
     },
